4d88f5c5b17dd1fe1c45e568269740053f9ca053
VLR-angular7 <59470943+VLR-angular7@users.noreply.github.com>
2020-01-13 11:40:54 +0530

    Create Angular_class2.ts

---
+++ Angular_class2.ts
@@ -0,0 +1,9 @@
+Angular project structure
+
+e2e : This folder contains the files for end-to-end testing, which is set up to use the Protractor package.
+
+---------------------------------------------------------------------------------------------------
+node_modules : This folder contains the NPM packages(dependencies described in package.json) that are required for the application 
+and for the Angular development tools(devdependencies).
+
+-------------------------------------------------------------------------------------------------------------
